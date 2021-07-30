2426967632d3feb94e93585605d7ab75fb751495
RyanVanDijck <rvandijck02@outlook.com>
2021-07-30 15:03:43 +0100

    Setting up npm project

---
+++ startup.sh
@@ -0,0 +1,14 @@
+npm init;
+
+npm install --save-dev express
+npm install --save-dev jasmine
+npm install --save-dev cypress
+npm install --save-dev ejs
+
+npx jasmine init
+
+
+mkdir views
+mkdir src
+mkdir spec
+npx cypress open
