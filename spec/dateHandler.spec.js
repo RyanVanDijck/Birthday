91df063bb7e148c22f898ca261e9cd558eacf691
RyanVanDijck <rvandijck02@outlook.com>
2021-07-30 15:55:07 +0100

    Fixing tests for real functionality

--- spec/dateHandler.spec.js
+++ spec/dateHandler.spec.js
@@ -2,21 +2,27 @@ const DateHandler = require('../src/dateHandler.js')
 
 describe('Date Tests', () => {
     it('Difference between dates', () =>{
-        const date1 = new Date('7/7/2021'); //7th of July
-        const date2 = new Date('7/6/2021'); //6th of July
-        const DateHandler = new DateHandler(date1, date2); 
-        expect(DateHandler.difference()).toEqual(1);
+        const date = new Date('7/6/2021'); //6th of July
+        const dateHandler = new DateHandler(6, 7); 
+        expect(dateHandler.difference()).toEqual(24);
     })
     it('Same Date', () => {
-        const date1 = new date('12/25/2018'); //Christmas
-        const date2 = new date('12/25/2018'); //Christmas Again
-        const DateHandler = new DateHandler(date1, date2); 
-        expect(DateHandler.difference()).toEqual(0);
-    })
-    it('Opposite Order', () => {
-        const date1 = new Date('7/6/2021'); //6th of July
-        const date2 = new Date('7/7/2021'); //7th of July
-        const DateHandler = new DateHandler(date1, date2); 
-        expect(DateHandler.difference()).toEqual(1);
+        const dateHandler = new DateHandler(new Date().getDate(), new Date().getMonth()+1); 
+        expect(dateHandler.difference()).toEqual(0);
     })
 }) 
+// 
+// var today = new Date();
+// var dd = today.getDate();
+
+// var mm = today.getMonth()+1; 
+// var yyyy = today.getFullYear();
+// if(dd<10) 
+// {
+//     dd='0'+dd;
+// } 
+
+// if(mm<10) 
+// {
+//     mm='0'+mm;
+// } today = mm+'-'+dd+'-'+yyyy;
