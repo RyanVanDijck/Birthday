bf7fda1cf87c4b5342264ccda90647d587020406
RyanVanDijck <rvandijck02@outlook.com>
2021-07-30 17:00:58 +0100

    Finishing Work

--- cypress/integration/name.spec.js
+++ cypress/integration/name.spec.js
@@ -1,5 +1,5 @@
 describe('page loads', () => {
-    it('displays name input', () => {
+    it('displays name input, input is submitted', () => {
         cy.visit('/')
         cy.get("#name").type('Lottie')
         cy.get("#submit").click()
--- routes/countDays.js
+++ routes/countDays.js
@@ -1,11 +1,23 @@
 var express = require('express')
 var router = express.Router()
+const DateHandler = require('../src/dateHandler.js')
 
 router.post('/', (req, res) => {
     const name = req.body.name
+    const day = req.body.somedate.split('-')[2]
+    const month = req.body.somedate.split('-')[1]
+    const dateHandler = new DateHandler(day,month)
+    var result
+    if (dateHandler.difference() === 0){
+        result = `Happy Birthday ${name}!!`
+    }
+    else{
+        result = `${name}, your birthday is ${dateHandler.difference()} days away`
+    }
     res.render('result.ejs', {
-        name: name
+        result: result
     })
 })
 
 module.exports = router
+
